7a344cdb22d95ae1b78c4e70f63137179bced305
Laís Barreto Vieira <92763262+laisbvieira@users.noreply.github.com>
2024-07-08 14:42:52 -0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import { log } from "./log.mjs";
 
 export const handler = async (event) => {
-  log("Log de execução" + JSON.stringify(event));
+  log("Log de execução após Git Actions" + JSON.stringify(event));
 
   return {
     statusCode: 200,
